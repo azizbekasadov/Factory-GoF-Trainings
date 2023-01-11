d2f1c6ef24529337c593b5f2d231505a2f28da87
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-11 20:50:40 +0500

    Initial Commit

---
+++ 2. Factory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2. Factory
+//
+//  Created by Azizbek Asadov on 11/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
